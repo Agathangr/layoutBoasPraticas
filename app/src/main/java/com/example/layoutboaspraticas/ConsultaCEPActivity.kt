e392b6f8d5173cdb26a7160fb377da7d8f53dfb3
DEVSESISENAI <agathangrodriges@gmail.com>
2024-06-27 15:59:36 -0300

    sheat: setup initial

---
+++ app/src/main/java/com/example/layoutboaspraticas/ConsultaCEPActivity.kt
@@ -0,0 +1,11 @@
+package com.example.layoutboaspraticas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsultaCEPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consulta_cepactivity)
+    }
+}
